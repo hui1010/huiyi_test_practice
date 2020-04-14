fb456f2d27a517dd44f7aa1cffb5a7c2834a5865
deltagare <huiyi.skarner@gmail.com>
2020-04-14 19:20:31 +0200

    make the user to input

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,12 +1,23 @@
 package se.lexicon;
 
 
-public class App 
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
     {
-        int a = 10, b = 5, result = 0;
-        char operator = '*';
+        double a = 0, b = 0, result = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the fist number: ");
+        a = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Please enter the second number: ");
+        b = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Please choose from (+ - * /)");
+        char operator = scanner.next().charAt(0);
+
         switch (operator) {
             case '+':
                 result = a + b;
@@ -23,6 +34,6 @@ public class App
             default:
                 System.out.println(operator + " is not supported");
         }
-        System.out.println( result );
+        System.out.println(result);
     }
 }
